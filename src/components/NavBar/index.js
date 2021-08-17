d90f9e922cc97fa56eda5297abc68f0a55a00e24
Henry Jones <jonesh@union.edu>
2021-08-16 22:42:14 -0400

    NavBar starting point

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import {
+    Nav, 
+    NavBarContainer, 
+    NavLogo, 
+    MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks,
+    NavBtn,
+    NavBtnLink
+    } from './NavBarElements.js';
+
+const NavBar = ({ toggle }) => {
+    return (
+       <>
+            <Nav>
+                <NavBarContainer>
+                    <NavLogo to='/'>Henry Jones | Portfolio</NavLogo>
+                    <MobileIcon onClick={toggle}>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="photohraphy">Photography</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="projects">Projects</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="hireMe">Hire Me</NavLinks>
+                        </NavItem>
+                        <NavBtn>
+                            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                        </NavBtn>
+                    </NavMenu>
+                </NavBarContainer>
+            </Nav>
+       </>
+    )
+}
+
+export default NavBar
---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+// import SideBar from '../components/SideBar';
+import NavBar from '../components/NavBar';
+// import HeroSection from '../components/HeroSection';
+
+
+const Home = () => {
+    // const [isOpen, setIsOpen] = useState(false)
+
+    // const toggle = () => {
+    //     setIsOpen(!isOpen)
+    // }
+
+    return (
+        // <div>Some More Text</div>
+        // <>
+        //     {/* <SideBar isOpen = {isOpen} toggle = {toggle} /> */}
+        //     {/* <NavBar toggle = {toggle} /> */}
+        //     {/* <HeroSection /> */}
+            <NavBar/>
+        // </>
+    )
+}
+
+export default Home
