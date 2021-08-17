f023c158438885eed73f9d75d594344bc232e22e
Henry Jones <jonesh@union.edu>
2021-08-16 22:50:18 -0400

    SideBar action working

---
+++ src/components/SideBar/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { 
+    SideBarContainer,
+    CloseIcon,
+    Icon,
+    SideBarLink,
+    SideBarRoute,
+    SideBarWrapper,
+    SideBarMenu,
+    SideBtnWrap
+ } from './SideBarElements'
+
+const SideBar = ({isOpen, toggle}) => {
+    return (
+        <SideBarContainer isOpen = {isOpen} onClick = {toggle}>
+            <Icon onClick = {toggle}>
+                <CloseIcon />
+            </Icon>
+            {/* <SideBarWrapper>
+                <SideBarMenu>
+                    <SideBarLink to ="about" onClick = {toggle}>About</SideBarLink>
+                    <SideBarLink to ="photography" onClick = {toggle}>Photography</SideBarLink>
+                    <SideBarLink to ="projects" onClick = {toggle}>Projects</SideBarLink>
+                    <SideBarLink to ="hireMe" onClick = {toggle}>Hire Me</SideBarLink>
+                </SideBarMenu>
+                    <SideBtnWrap>
+                        <SideBarRoute to="/signin">Sign In</SideBarRoute>
+                    </SideBtnWrap>
+            </SideBarWrapper> */}
+        </SideBarContainer>
+    )
+}
+
+export default SideBar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,24 @@
 import React, {useState} from 'react';
-// import SideBar from '../components/SideBar';
+import SideBar from '../components/SideBar';
 import NavBar from '../components/NavBar';
 // import HeroSection from '../components/HeroSection';
 
 
 const Home = () => {
-    // const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
 
-    // const toggle = () => {
-    //     setIsOpen(!isOpen)
-    // }
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
 
     return (
         // <div>Some More Text</div>
-        // <>
-        //     {/* <SideBar isOpen = {isOpen} toggle = {toggle} /> */}
-        //     {/* <NavBar toggle = {toggle} /> */}
-        //     {/* <HeroSection /> */}
-            <NavBar/>
-        // </>
+        <>
+            <SideBar isOpen = {isOpen} toggle = {toggle} /> 
+            <NavBar toggle = {toggle} />
+            {/* <HeroSection /> */}
+            {/* <NavBar/> */}
+        </>
     )
 }
 
