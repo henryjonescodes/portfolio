a84a827f8e7e1240b440c53be5d714547879dbe4
Henry Jones <jonesh@union.edu>
2021-10-20 12:37:12 -0400

    added some home elements back and implemented three on top of react

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import LinkBar from '../components/Common/LinkBar'
 import RoutedSideBar from '../components/Common/RoutedSideBar'
 import { theme } from '../components/Home/theme'
+import HeroSection from '../components/Home/HeroSection';
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -14,6 +15,7 @@ const Home = () => {
         <>
             <RoutedSideBar isOpen = {isOpen} toggle = {toggle} theme = {theme}/>
             <LinkBar toggle = {toggle} title = "Home" theme = {theme} sticky ={true}/>
+            <HeroSection/>
         </>
     )
 }
