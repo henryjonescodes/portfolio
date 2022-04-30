337580ad00f7ff0a0b49c61c27b3eb8a49a112b9
Henry Jones <89033121+henryjonescodes@users.noreply.github.com>
2022-04-30 11:16:43 -0700

    Setup Vite + React + Sass + Inline SVG

---
+++ src/vite-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+// declare module '*.svg' {
+//   import * as React from 'react';
+//   export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+//   const src: string;
+//   export default src;
+// }
