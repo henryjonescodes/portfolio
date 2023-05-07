c740c4c2e1277dcb056bb25e11d26b23c8e89267
Henry Jones <89033121+henryjonescodes@users.noreply.github.com>
2023-05-06 18:43:48 -0700

    Initial Commit

--- src/App2.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const App2 = () => {
-  return <div>app</div>
-}
-
-export default App2
--- src/pages/loading-animations/__template/AnimationOne/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const AnimationOne = () => {
-  return <div>AnimationOne</div>
-}
-
-export default AnimationOne
