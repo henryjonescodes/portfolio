f63829147b26afb175180b63bfc6fd179aa6ace8
Henry Jones <89033121+henryjonescodes@users.noreply.github.com>
2024-05-04 11:00:31 -0700

    Framer Motion x Styles Homepage Pass
    Framer??
    
    Style pass 1
    
    Pretty neat
    
    More!
    
    Big Progress

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from './home-components.module.scss'
+import background from '@assets/landing-background.png'
 
 const Hero = () => {
   return (
     <div className={styles.hero}>
       <h1>Henry Jones</h1>
-      <h2>Creative Developer</h2>
+      <h3>Creative Developer</h3>
+      <img src={background} />
     </div>
   )
 }
