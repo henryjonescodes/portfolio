a5d7a6ddd3a7d5ae55ff24c825533e1d1d4a961c
Henry Jones <89033121+henryjonescodes@users.noreply.github.com>
2024-06-08 14:36:11 -0700

    Favicon, Assets, Tweaks
    Links, Favicons
    
    Images and mobile tweaks
    
    Vibes
    
    Favicon!
    
    Tweaks
    
    Heights

--- src/constants/links.constants.ts
+++ src/constants/links.constants.ts
@@ -2,5 +2,5 @@
 export const LINKEDIN_PROFILE_URL = "https://www.linkedin.com/in/henryjonescodes/"
 export const GITHUB_PROFILE_URL = "https://github.com/henryjonescodes/"
 export const INSTAGRAM_PROFILE_URL = "https://www.instagram.com/theycallmezonez/"
-export const CALENDAR_URL = "https://calendar.google.com" // TODO: add calender
+export const CALENDAR_URL = "https://calendly.com/henryjonescodes"
 export const EMAIL_URL = "mailto:henryjonescodes@gmail.com"
