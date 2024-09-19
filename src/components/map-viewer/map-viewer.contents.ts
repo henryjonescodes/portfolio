9667ae3c4d4bd06eef7771921c0224d1bd4b067a
Henry Jones <89033121+henryjonescodes@users.noreply.github.com>
2024-09-19 14:44:45 -0400

    Update Resume and Map

--- src/components/map-viewer/map-viewer.contents.ts
+++ src/components/map-viewer/map-viewer.contents.ts
@@ -2,29 +2,29 @@ import Channel from '@assets/jobs/channel-icon.svg'
 import Mushroom from '@assets/jobs/mushroom-icon.svg'
 import Tumblr from '@assets/jobs/tumblr-icon.svg'
 import Book from '@assets/svg/icons/book.svg'
-import House from '@assets/svg/icons/house.svg'
+import Building from '@assets/svg/icons/building.svg'
 import { PointOfInterest } from './types'
 
 
 
 export const locationData: Record<'portland' | 'paloAlto' | 'nyc' | 'schenectady', PointOfInterest> =
   {
-    // portland: {
-    //   prefix: 'I grew up in',
-    //   title: 'Portland, Maine',
-    //   description:
-    //     'I grew up in the northeast in coastal Maine. I spent my time sailing off rocky shores, hiking and biking around pristine mountains, lakes, and rivers, and cooking at various restaurants throughout Portland’s diverse restaurant scene.',
-    //   mapTitle: 'PORTLAND, ME',
-    //   mapHighlights: [{ icon: Book, text: 'Casco Bay HS' }],
-    // },
     portland: {
-      prefix: 'I’m currently based in',
+      prefix: 'I grew up in',
       title: 'Portland, Maine',
       description:
-        'I’m searching for my next professional adventure in software, enjoying summer on the coast with family and friends, and getting some of my personal projects off of my backlog.',
+        'I grew up in the northeast in coastal Maine. I spent my time sailing off rocky shores, hiking and biking around pristine mountains, lakes, and rivers, and cooking at various restaurants throughout Portland’s diverse restaurant scene.',
       mapTitle: 'PORTLAND, ME',
-      mapHighlights: [{ icon: House, text: 'Home' }],
+      mapHighlights: [{ icon: Book, text: 'Casco Bay HS' }],
     },
+    // portland: {
+    //   prefix: 'I’m currently based in',
+    //   title: 'Portland, Maine',
+    //   description:
+    //     'I’m searching for my next professional adventure in software, enjoying summer on the coast with family and friends, and getting some of my personal projects off of my backlog.',
+    //   mapTitle: 'PORTLAND, ME',
+    //   mapHighlights: [{ icon: House, text: 'Home' }],
+    // },
     paloAlto: {
       prefix: 'I make apps in',
       title: 'Palo Alto, California',
@@ -37,12 +37,15 @@ export const locationData: Record<'portland' | 'paloAlto' | 'nyc' | 'schenectady
       ],
     },
     nyc: {
-      prefix: 'I interned at Tumblr in',
+      prefix: 'I’m on the job market in',
       title: 'New York City',
       description:
-        'After an internship with Tumblr early on my journey into software and a stint living in Manhattan during college, I love NYC. I’m currently in process of moving from California, destination Brooklyn.',
+        'On the lookout for my next challenge in New York City, where I can apply my expertise in React, TypeScript, and frontend development to build innovative, seamless user experiences. Working on my next big idea in the meantime',
       mapTitle: 'NEW YORK, NY',
-      mapHighlights: [{ icon: Tumblr, text: 'Tumblr' }],
+      mapHighlights: [
+        // { icon: Tumblr, text: 'Tumblr' },
+        { icon: Building, text: 'Your Company?' }
+      ],
     },
     schenectady: {
       prefix: 'I went to Union College in',
