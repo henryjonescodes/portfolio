9d03c024efc408781998a20d416d66164f2e0b12
Henry Jones <89033121+henryjonescodes@users.noreply.github.com>
2024-11-05 13:24:44 -0500

    Sass Colors Update Logic

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -5,32 +5,56 @@ import Page from "../../components/Page";
 import { useSettings } from "../../context/SettingsContext";
 import styles from "./landing.module.scss";
 import Scene from "./Scene";
+import { useControls } from "leva";
+import { useEffect } from "react";
+import { colors as defaultColors } from "./../../styles/sass-variables";
+
+const wrapperVariants = {
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 1.3,
+      delay: 0.6,
+    },
+  },
+  hide: {
+    opacity: 0,
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
 
 const Landing = () => {
   const location = useLocation();
-  const pathSegments = location.pathname.split("/").filter(Boolean);
-  const page = pathSegments[0];
-
   const navigate = useNavigate(); // Initialize the navigate function
   const { fullScreen } = useSettings();
 
+  const pathSegments = location.pathname.split("/").filter(Boolean);
+  const page = pathSegments[0];
   const isHidden = !fullScreen || page === undefined;
 
-  const wrapperVariants = {
-    show: {
-      opacity: 1,
-      transition: {
-        duration: 1.3,
-        delay: 0.6,
-      },
-    },
-    hide: {
-      opacity: 0,
-      transition: {
-        duration: 0.3,
-      },
-    },
-  };
+  const colors = useControls({
+    "foreground-primary": defaultColors["foreground-primary"],
+    "foreground-secondary": defaultColors["foreground-secondary"],
+    "foreground-tertiary": defaultColors["foreground-tertiary"],
+    "foreground-quaternary": defaultColors["foreground-quaternary"],
+    "accent-primary": defaultColors["accent-primary"],
+    "accent-secondary": defaultColors["accent-secondary"],
+    "accent-tertiary": defaultColors["accent-tertiary"],
+    "background-primary": defaultColors["background-primary"],
+    "background-secondary": defaultColors["background-secondary"],
+  });
+
+  useEffect(() => {
+    const root = document.documentElement;
+
+    Object.entries(colors).forEach(([key, value]) => {
+      root.style.setProperty(`--${key}`, value);
+    });
+
+    console.log("CSS variables updated with new colors");
+  }, [colors]); //
 
   return (
     <motion.div className={styles.landing}>
---
+++ src/styles/sass-variables.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  "foreground-primary": "#00d67d",
+  "foreground-secondary": "#0bcb7b",
+  "foreground-tertiary": "#2bab76",
+  "foreground-quaternary": "#4b8b70",
+  "accent-primary": "#003333",
+  "accent-secondary": "#0f2424",
+  "accent-tertiary": "#0f2424",
+  "background-primary": "#003d24",
+  "background-secondary": "rgb(0, -41, -23.9485981308)"
+} as const;
